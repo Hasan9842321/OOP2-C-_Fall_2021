1b052b2ff6c59c4789c5d7b7d865df218a585cb7
Hasan9842321 <abeer.rocky@gmail.com>
2022-08-23 19:16:08 +0600

    C# Project: HealthCare Management Syatem

---
+++ HealthCareManagementSystem/HealthCareManagementSystem/DataAccessLayer/Registration.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HealthCareManagementSystem.DataAccessLayer
+{
+    class Registration
+
+    {
+        public int RegSlNo { get; set; }
+    
+        public string Username { get; set; }
+        public string Gender { get; set; }
+       
+        public string Email { get; set; }
+        public string Number { get; set; }
+        public string Address { get; set; }
+        public int Age { get; set; }
+
+        
+    }
+}
